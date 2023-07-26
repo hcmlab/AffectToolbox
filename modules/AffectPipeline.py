752410dc448d9181d36eb307e12366c4b585151d
Florian Lingenfelser <47107182+florianlingenfelser@users.noreply.github.com>
2023-07-26 15:57:46 +0200

    Revert "typo"
    This reverts commit 068cfdf38ba5288ec77967a9b9482102f9b70258.

--- modules/AffectPipeline.py
+++ modules/AffectPipeline.py
@@ -6,7 +6,7 @@ import threading
 import cv2
 import soundfile
 from modules.VideoDecorator import VideoShow
-from modules.logger import LogModule
+from modules.Logger import LogModule
 import socket
 import lxml.etree
 import lxml.builder
