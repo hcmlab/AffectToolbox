18be18d103c2aa4140e9b901f044db95923202e0
Florian Lingenfelser <47107182+florianlingenfelser@users.noreply.github.com>
2024-06-06 12:18:08 +0200

    sub_main with ClickEvents

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from GUI.main import MainWindow
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QIcon
+from GUI import ClickEvents
 
-def main():
+def sub_main():
     """Main function to run the application"""
     app = QApplication([])
     window = MainWindow()
@@ -10,7 +11,21 @@ def main():
     window.setWindowIcon(QIcon("./GUI/Icons/python.svg"))
     window.show() #Windowed mode
     #window.showFullScreen() #Fullscreen mode
+
+    ClickEvents.PleasureClick(window)
+    ClickEvents.ArousalClick(window)
+    ClickEvents.DominanceClick(window)
+    ClickEvents.ParaPleasureClick(window)
+    ClickEvents.ParaArousalClick(window)
+    ClickEvents.ParaDominanceClick(window)
+    ClickEvents.VoiceActivityClick(window)
+
+    ClickEvents.PlayButtonClick(window)
+
     app.exec()
+
+def main():
+    sub_main()
     
 if __name__ == "__main__":
     main()
