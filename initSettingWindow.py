fc974090d56b44e4252d7c0838f3bbf8d11660bc
claudia165 <claudia.ehrmann@uni-a.de>
2025-06-07 14:17:28 +0200

    Delete initSettingWindow.py

--- initSettingWindow.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtCore import Qt
-from rightClickWindow import RightClickWindow
-
-def InitSettingWindow(button):
-    # Open right-click settings window for a specific widget
-    dlg = RightClickWindow(name=button.base_text)
-    dlg.setWindowTitle(f"{button.base_text} Settings")
-    dlg.setWindowModality(Qt.ApplicationModal)
-    dlg.exec()  # blocked until window is closed
